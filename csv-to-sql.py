64bfaeb8fcbaab3365d3804fc2b90ded891f32be
Jordan Evans <jevans17@uoguelph.ca>
2019-02-12 18:50:12 -0500

    Added feature to change output sql column names

--- csv-to-sql.py
+++ csv-to-sql.py
@@ -5,35 +5,104 @@
 # 
 # Arguments:
 #   - path to csv file
-#   - path to sql export file
-#   - sql table name
-#   - column headers (as many as you want)
+#   - [-t]  -> sql table name
+#   - [-ch] -> column headers (as many as you want) NOTE: Headers cannot start with '-'
 #
+# Optional Arguments:
+#   - [-o]  -> output file name
+#   - [-sh] -> sql headers. if headers from csv to sql arent consistant
+#              this use this list of headers for the sql output 
+
 
 import sys, csv;
 
 # Validate command line arguments
 def validateArguments(args):
-	if(len(args) < 5):
+
+	if(args['in_fn'][-4:] != '.csv' and args['in_fn'][-4:] != '.CSV'):
+		print("Invalid input filename. Must end in '.csv' or '.CSV'.");
 		return False;
+
+	# Output filename validation
+	if 'out_fn' in args:
+		if(args['out_fn'][-4:] != '.sql' and args['out_fn'][-4:] != '.SQL'):
+			print("Invalid output filename. Must end in '.sql' or '.SQL'.");
+			return False;
+
+	# Table name validation
+	if 'table' not in args:
+		print("Invalid arguments. No table name provided.");
+		return False;
+	else:
+		if(args['table'] == ""):
+			print("Invalid arguments. Empty table name provided.");
+			return False;
+
+	# csv header validation
+	if 'csv_headers' not in args:
+		print("Invalid arguments. No CSV headers provided.");
+		return False;
+
+	# sql header validation
+	if 'sql_headers' in args:
+		if(len(args['csv_headers']) != len(args['sql_headers'])):
+			print(args['csv_headers']);
+			print(args['sql_headers']);
+
+			print("Invalid arguments. Number of input headers is not equal to number of output headers.");
+			return False;
+
 
 	return True;
 
-
 # Parse column headers from cmd line arguments
-def parseColumnHeaders(args):
-	headers = [];
-	for i in range(4, len(args)):
-		headers.append(args[i]);
-	return headers;
+def parseArguments(args):
+
+	params = {};
+
+	params['in_fn'] = args[1];
+
+	for i in range(0,len(args)):
+		if(args[i] == "-o"):
+			params['out_fn'] = args[i+1];
+		elif(args[i] == '-t'):
+			params['table'] = args[i+1];
+		elif(args[i] == '-ch'):
+			j = 1;
+			csv_head_arr = [];
+			csv_cur_arg = args[i+j];
+			while(csv_cur_arg[0] != '-'):
+				csv_head_arr.append(csv_cur_arg);
+				j += 1;
+				csv_cur_arg = args[i+j];
+			params['csv_headers'] = csv_head_arr;
+		elif(args[i] == '-sh'):
+			k = 1;
+			sql_head_arr = [];
+			sql_cur_arg = args[i+k];
+			while(sql_cur_arg[0] != '-'):
+				sql_head_arr.append(sql_cur_arg);
+				k += 1;
+				if(i + k < len(args)):
+					sql_cur_arg = args[i+k];
+				else:
+					break;
+			params['sql_headers'] = sql_head_arr;
+
+	return params;
 
 # Match headers from arguments to headers from csv
 def getHeaderIndexFromCSV(csv, headers):
 	indecies = [];
 
+	for i in range(0, len(headers)):
+		indecies.append(-1);
+
 	for i in range(0, len(csv[0])):
 		if csv[0][i] in headers:
-			indecies.append(i);
+			cur_header = csv[0][i];
+			ind = headers.index(cur_header)
+			indecies[ind] = i;
 
 	return indecies;
 
@@ -77,43 +146,71 @@ def createSQL(csv, table, indecies, headers):
 # Export SQL
 
 # Test argument print
-def testArgPrint(csv, sql, table, headers):
-	header_str = "";
-	
+def testArgPrint(csv, sql, table, headers, sql_headers, ind):
+	header_str     = "";
+	ind_str        = "";
+	sql_header_str = "";
+
 	for h in headers:
 		header_str += str(h) + ", ";
+
+	for i in ind:
+		ind_str += str(i) + ", ";
+
+	for sh in sql_headers:
+		sql_header_str += str(sh) + ", ";
 
 	print("CSV: "+ str(csv));
 	print("SQL: "+ str(sql));
 	print("Table: "+ str(table));
-	print("Headers: " + str(header_str));
+	print("Indecies: "+ str(ind_str));
+	print("CSV Headers: " + str(header_str));
+	print("SQL Headers: "+ str(sql_header_str));
 
 
 # Test Main
 def testMain(args):
 
-	if (not validateArguments(args)):
-		print("Invalid command line arguments.");
-		return;
+	params = parseArguments(args);
 
-	csv_filename = args[1];
-	sql_filename = args[2];
-	table_name   = args[3];
-	col_headers  = parseColumnHeaders(args);
+	if not validateArguments(params): return;
+	
+	is_sql_headers = False;
+	is_out_file    = False;
 
-	testArgPrint(csv_filename, sql_filename, table_name, col_headers);
+	csv_filename = params['in_fn'];
+	table_name   = params['table'];
+	csv_headers  = params['csv_headers'];
+	sql_filename = "";
+	sql_headers  = [];
+
+
+	if 'out_fn' in params:
+		is_out_file = True;
+		sql_filename = params['out_fn'];
+	if 'sql_headers' in params:
+		is_sql_headers = True;
+		sql_headers = params['sql_headers'];
 
 	csv = importCSV(csv_filename);
 
-	indecies = getHeaderIndexFromCSV(csv, col_headers);
+	indecies = getHeaderIndexFromCSV(csv, csv_headers);
 
 	if(len(indecies) == 0):
 		print("No column headers match csv headers");
 		return;
 
-	sql_content = createSQL(csv, table_name, indecies, col_headers);
+	sql_content = "";
+
+	if is_sql_headers:
+		sql_content = createSQL(csv, table_name, indecies, sql_headers);
+	else:
+		sql_content = createSQL(csv, table_name, indecies, csv_headers);
 
 	print(sql_content);
+
+	testArgPrint(csv_filename, sql_filename, table_name, csv_headers, sql_headers, indecies);
+
 
 
 	
